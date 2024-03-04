2bbe31118ffd2313b91cd79b67e21fdaa74de64a
vthuhien <142284485+vthuhien@users.noreply.github.com>
2024-03-04 10:46:55 +0700

    Delete js directory

--- js/thuchanh/animation/1.js
+++
@@ -1,8 +0,0 @@
-var h1 = document.querySelector('#home h1')
-
-//onscroll - cứ xoay con trỏ chuột thì in ra chứ test
-window.onscroll = function () {
-    var recH1 = h1.getClientRects()
-    console.log(recH1)
-}
-
